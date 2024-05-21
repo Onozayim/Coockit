4c8b05e8a7e8e6d102af92467b5a261d3794421c
Onozayim <camilomdo@gmail.com>
2024-05-20 19:07:30 -0600

    Lowing titlte

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,13 +6,16 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('styles/loginstyle.css') }}">
+    <link rel="stylesheet" href="{{ asset('styles/registerstyle.css') }}">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <title>coockIt</title>
 </head>
 <body>
 <div class="parent">
     <div class="div1"> 
-        <h1>LOGIN</h1>
+        <div class="full_div">
+            <h1>LOGIN</h1>
+        </div>
     </div>
 
     <div class="div2"> 
