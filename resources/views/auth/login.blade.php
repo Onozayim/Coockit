dec0fd5e40067f5fc988e1755bcdfb9f5b3d568d
Diego <diegomaximiliano1203@gmail.com>
2024-05-20 18:32:29 -0600

    Login y Register con css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,27 +5,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('styles/loginstyle.css') }}">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <title>coockIt</title>
 </head>
 <body>
-    <h1>LOGIN</h1>
+<div class="parent">
+    <div class="div1"> 
+        <h1>LOGIN</h1>
+    </div>
 
-    <form action="{{route('authenticate')}}" method="post">
-        @csrf
-        <input name="email" type="text" placeholder="Ingrese su correo">
-        <input name="password" type="password" placeholder="Ingrese su contraseña">
+    <div class="div2"> 
+        <form action="{{route('authenticate')}}" method="post">
+            @csrf
+            <input name="email" type="text" placeholder="Ingrese su correo">
+            <input name="password" type="password" placeholder="Ingrese su contraseña">
 
-        <button type="submit">Enviar</button>
+            <button type="submit">Enviar</button>
 
-        @if ($errors->any())
-        <div class="alert alert-danger">
-            <p>
-                {{$errors->first()}}
-            </p>
-        </div>
-    @endif
-    </form>
-    <a href="{{ route('register') }}">No tienes cuenta? crea una</a>
+            @if ($errors->any())
+            <div class="alert alert-danger">
+                <p>
+                    {{$errors->first()}}
+                </p>
+            </div>
+        @endif
+        </form>
+    </div>
+
+    <div class="div3"> 
+        <a href="{{ route('register') }}">No tienes cuenta? crea una</a>
+    </div>
+</div>
 </body>
 </html>
